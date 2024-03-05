311c5550be8ae5359fe568359d15f462dcb5ebf8
jkktaglineinfotech <jkk@taglineinfotech.com>
2024-03-05 14:48:48 +0530

    Code for practice.

--- src/container/user.container.js
+++ src/container/user.container.js
@@ -8,64 +8,24 @@ import {
   stopLoadingSuccess,
 } from "../redux/actions/loadingAction";
 
+let users = "John";
+let stateUpdated = false;
 export const userContainer = () => {
-  const [counter, setCounter] = useState(0);
-
-  const [usersData, setUsersData] = useState([]);
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
-  const [usersPerPage, setUsersPerPage] = useState(5);
-
-  const dispatch = useDispatch();
-  useEffect(() => {
-    // Merge of componentDidMount and componentDidUpdate
-    console.log("It will run whenever component mounts or updates");
-  });
-
-  const fetchUsers = async () => {
-    dispatch(startLoading());
-    const { data, page, per_page, total, total_pages } = await getUsers(
-      currentPage,
-      usersPerPage
-    );
-    if (!data) {
-      dispatch(stopLoadingError());
-      return;
-    }
-    const finalData = data.map((user) => formatUserData(user));
-    setUsersData(finalData);
-    setTotalPages(total_pages);
-    dispatch(stopLoadingSuccess());
+  const onFunction = () => {
+    users = "Jason";
+    stateUpdated = true;
+    console.log("Function Called.", users, stateUpdated);
   };
+  console.log(users === "John" ? "Test" : "Test User");
+  console.log("Test.", users, stateUpdated);
 
-  useEffect(() => {
-    // Equivalent of componentDidMount
-    console.log("This message is only showed once in whole lifecycle");
-    // fetchUsers();
-  }, []);
-
-  // Merge of componentDidMount and componentDidUpdate but only for given dependency
-  useEffect(() => {
-    console.log(`This is mounted or count state updated => ${currentPage} .`);
-    fetchUsers();
-  }, [currentPage]);
-
-  // Equivalent of componentWillUnmount
-  useEffect(() => {
-    return () => {
-      console.log("This is unmounted.");
-    };
-  }, []);
-
-  const handleOnPageChange = ({ target }) => {
-    console.log(parseInt(target?.outerText));
-    setCurrentPage(parseInt(target?.outerText));
+  const runOnUserValueChange = () => {
+    console.log("Wehnever user's value changed, it calls .");
   };
 
   return {
-    usersData,
-    totalPages,
-    handleOnPageChange,
+    users,
+    onFunction,
+    stateUpdated,
   };
 };
--- src/presentation/Home.jsx
+++ src/presentation/Home.jsx
@@ -1,24 +1,59 @@
-import React from "react";
-import logo from "../logo.svg";
-import { userContainer } from "../container/user.container";
-import Counter from "./components/Counter";
-import CommonDataTable from "../shared/CommonDataTable";
-import { Pagination } from "../shared/Pagination";
-import { projectText } from "../description/user.description";
+import React, { useEffect, useRef, useState } from "react";
+
+let users = "John";
+let stateUpdated = false;
 
 const Home = () => {
-  const { usersData, handleOnPageChange, totalPages } = userContainer();
+  const userRef = useRef();
+  const buttonRef = useRef();
+
+  const [event, setEvent] = useState(null);
+  const onFunction = () => {
+    users = "Jason";
+    stateUpdated = true;
+    setEvent(users);
+    userRef.current.innerHTML = users;
+    buttonRef.current.innerHTML = users;
+    buttonRef.current.disabled = stateUpdated;
+    console.log("onFunction", users);
+  };
+
+  // useEffect(() => {
+  //   if (event) console.log("Event", event);
+  //   return () => {
+  //     setEvent(null);
+  //   };
+  // }, [event]);
+
+  useEffect(() => {
+    console.log("Effect Normal", users);
+  }, []);
   return (
     <>
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1>{projectText}</h1>
-        {usersData.length > 1 && <CommonDataTable data={usersData} />}
-        {/* <Counter counter={counter} setCounter={setCounter} /> */}
-        <Pagination pages={totalPages} onClick={handleOnPageChange} />
+        <h1 ref={userRef}>{users}</h1>
+        <button
+          ref={buttonRef}
+          type="button"
+          onClick={onFunction}
+          disabled={stateUpdated}
+        >
+          {users}
+        </button>
       </header>
     </>
   );
 };
 
 export default Home;
+
+// console.log("Function Called.", users, stateUpdated);
+// forceUpdate();
+
+// console.log(users === "John" ? "Test" : "Test User");
+// console.log("Test.", users, stateUpdated);
+
+// const [, forceUpdate] = useReducer((x) => x + 1, 0);
+// console.log("Ref", homeRef.current.innerHTML);
+
+// console.log(buttonRef.current);
