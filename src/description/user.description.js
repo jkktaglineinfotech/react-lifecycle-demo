c088f5b9f2f13ced436b2826cb46daf873ce18c6
jkktaglineinfotech <jkk@taglineinfotech.com>
2024-03-05 18:24:35 +0530

    Code Changes for Dynamic table.

--- src/description/user.description.js
+++ src/description/user.description.js
@@ -3,3 +3,16 @@ export const userApiEndPoints = {
 };
 
 export const projectText = "Demo Project";
+
+export const staticDataForTable = [
+  { id: 1, first_name: "Test 1", last_name: "user 1" },
+  { id: 2, first_name: "Test 2", last_name: "user 2" },
+  { id: 3, first_name: "Test 3", last_name: "user 3" },
+  { id: 4, first_name: "Test 4", last_name: "user 4" },
+  { id: 5, first_name: "Test 5", last_name: "user 5" },
+  { id: 6, first_name: "Test 6", last_name: "user 6" },
+  { id: 7, first_name: "Test 7", last_name: "user 7" },
+  { id: 8, first_name: "Test 8", last_name: "user 8" },
+  { id: 9, first_name: "Test 9", last_name: "user 9" },
+  { id: 10, first_name: "Test 10", last_name: "user 10" },
+];
